3132c2755bdee049c0fa9b1a04cca83a50acf3e6
Bradai Mohamed Habib <hbibbradai3@gmail.com>
2019-07-18 19:49:40 +0100

    Assignment 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -6,7 +6,7 @@ import {ContactComponent} from '../contact/contact.component';
 import {DishdetailComponent} from '../dishdetail/dishdetail.component';
 export const routes: Routes =[
 { path:'home', component:HomeComponent},
-{ path :'about', component:AboutComponent},
+{ path :'aboutus', component:AboutComponent},
 { path : 'menu', component:MenuComponent},
 { path: 'contactus',     component: ContactComponent },
 { path :'',  redirectTo: '/home', pathMatch: 'full'}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import {PromotionService } from '../services/promotion.service';
 import {DishService} from '../services/dish.service';
 import { Dish } from '../shared/dish';
 import { Promotion} from '../shared/promotion';
-
+import { Leader} from '../shared/leader'
+import { LeaderService } from '../services/leader.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,12 +13,15 @@ import { Promotion} from '../shared/promotion';
 export class HomeComponent implements OnInit {
 dish : Dish ;
 promotion : Promotion ;
+leader : Leader;
   constructor( private promotionService: PromotionService,
-  private dishService: DishService) { }
+  private dishService: DishService,
+  private leaderService: LeaderService) { }
 
   ngOnInit() {
     this.dish= this.dishService.getFeaturedDish();
     this.promotion= this.promotionService.getFeaturedPromotion();
+	this.leader= this.leaderService.getFeaturedLeader();
   }
 
 }
