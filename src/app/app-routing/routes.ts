c02d4953825c2b17b29935c480a3bdb3a1796136
Bradai Mohamed Habib <hbibbradai3@gmail.com>
2019-07-19 13:58:14 +0100

    Promise Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,6 +9,6 @@ export const routes: Routes =[
 { path :'aboutus', component:AboutComponent},
 { path : 'menu', component:MenuComponent},
 { path: 'contactus',     component: ContactComponent },
-{ path :'',  redirectTo: '/home', pathMatch: 'full'}
-
+{ path :'',  redirectTo: '/home', pathMatch: 'full'},
+{ path: 'dishdetail/:id',     component: DishdetailComponent },
 ]
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit , Input} from '@angular/core';
 import {Dish} from '../shared/dish'
+import { DishService} from '../services/dish.service';
+import { Params,ActivatedRoute} from '@angular/router';
+import { Location}from '@angular/common'
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -7,11 +11,28 @@ import {Dish} from '../shared/dish'
 })
 
 export class DishdetailComponent implements OnInit {
-    @Input()
+
     dish : Dish;
-  constructor() { }
+    dishIds: string[];
+  prev: string;
+  next: string;
+  constructor(private dishService: DishService,
+  private location: Location,
+private route: ActivatedRoute) { }
   
   ngOnInit() {
+    this.dishService.getDishIds().subscribe(dishIds => this.dishIds= dishIds)
+    this.route.params.pipe(switchMap((params: Params)=> this.dishService.getDish(params['id']))).subscribe(
+      dish => {this.dish=dish ;this.setPrevNext(dish.id) }
+    )
+  
   }
-
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
